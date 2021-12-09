9dc4f713b525c916fd9bedc292feb30f1fd2e1a4
Johanna Häggman <johanna@noordinaryscent.com>
2021-12-09 09:29:25 +0100

    comments deleted/ changed

--- src/units/gameLogic.js
+++ src/units/gameLogic.js
@@ -45,46 +45,32 @@ export function isSolvable(tiles) {
     const shuffledTiles = [
       ...tiles
         .filter((t) => t !== tiles.length - 1)
-        //sorterar rutorna random. 
         .sort(() => Math.random() - 0.5),
       tiles.length - 1,
     ];
-    //Kollar så att det går att lösa och ser till att den inte returnerar ett pussel som redan är löst. 
     return isSolvable(shuffledTiles) && !isSolved(shuffledTiles)
       ? shuffledTiles
-      //annars (:) shuffla om. 
       : shuffle(shuffledTiles);
   }
 
+  //Funktion för att byta plats på pjäser
   export function swap(tiles, index) {
     let tilesResult = [...tiles];
-    //lediga luckan
     let emptyIndex = tilesResult.indexOf(8);
-    //Klickade rutan
     let targetIndex = tilesResult.indexOf(index);
-    //Avståndet mellan lediga luckan och klickade luckan 
     const dif = Math.abs(targetIndex - emptyIndex);
-    //Få raden och kolumen för den klickade rutan
     let indexPosition = getMatrixPosition(targetIndex);
-    //Få raden och kolumnen för den tomma rutan 
     let emptyPosition = getMatrixPosition(emptyIndex);
-    //Om klickade rutans kolumn index = tomma rutans kolumn index, eller klickade rutans rad index = tomma rutans rad index
     if (indexPosition.col === emptyPosition.col || indexPosition.row === emptyPosition.row) {
-      //Om diffen är 1 eller 3 så är klickade och tomma brevid varanda och vi byter plats på dem
       if (dif === 1 || dif === 3) {
         tilesResult[emptyIndex] = index;
         tilesResult[targetIndex] = 8;
       }
-      //om dif är 2 eller 6 så är de ett steg ifrån varandra. 
       if (dif === 2 || dif === 6) {
-        //klickade rutans index + tomma rutan index / 2 
         const middleIndex = Math.round((targetIndex + emptyIndex)/2);
         const middleValue = tilesResult[middleIndex];
-        //Lediga rutan får middleValue dvs värdet emellan dem 
         tilesResult[emptyIndex] = middleValue;
-        //mellan rutan får klickade rutans värde
         tilesResult[middleIndex] = index;
-        //klickade rutan hoppar till tomma rutan. 
         tilesResult[targetIndex] = 8;
       }
     }
